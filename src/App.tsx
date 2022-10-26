f416c6c3440ace84303e034b5548b7ed91ed3ba0
Luukas Lehtinen <lukituubi@gmail.com>
2022-10-26 21:07:12 +0300

    create board

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import "./styles.css"
+
 const App = () => {
   return (
-    <div>Tic Tac Toe</div>
+    <div className="board">
+      <div className="top-left-square"></div>
+      <div className="top-mid-square"></div>
+      <div className="top-right-square"></div>
+      <div className="mid-left-square"></div>
+      <div className="mid-mid-square"></div>
+      <div className="mid-right-square"></div>
+      <div className="bottom-left-square"></div>
+      <div className="bottom-mid-square"></div>
+      <div className="bottom-right-square"></div>
+    </div>
   )
 }
 
