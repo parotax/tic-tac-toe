a8c1cbe4c3b1bd86d6874a04351cd9e12b65233f
Luukas Lehtinen <lukituubi@gmail.com>
2022-11-24 15:48:58 +0200

    Optimize Board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,15 +3,15 @@ import "../styles.css";
 const Board = () => {
   return (
     <div className="board">
-      <div className="top-left-square"></div>
-      <div className="top-mid-square"></div>
-      <div className="top-right-square"></div>
-      <div className="mid-left-square"></div>
-      <div className="mid-mid-square"></div>
-      <div className="mid-right-square"></div>
-      <div className="bottom-left-square"></div>
-      <div className="bottom-mid-square"></div>
-      <div className="bottom-right-square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
     </div>
   );
 };
