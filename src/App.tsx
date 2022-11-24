450278771745644d3c94f066bddfc48fa9ec852a
Luukas Lehtinen <lukituubi@gmail.com>
2022-11-24 15:40:44 +0200

    Move board to it's own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
 import "./styles.css";
+import Board from "./components/Board";
 
 const App = () => {
-  return (
-    <div className="board">
-      <div className="top-left-square"></div>
-      <div className="top-mid-square"></div>
-      <div className="top-right-square"></div>
-      <div className="mid-left-square"></div>
-      <div className="mid-mid-square"></div>
-      <div className="mid-right-square"></div>
-      <div className="bottom-left-square"></div>
-      <div className="bottom-mid-square"></div>
-      <div className="bottom-right-square"></div>
-    </div>
-  );
+  return <Board />;
 };
 
 export default App;
---
+++ src/components/Board.tsx
@@ -0,0 +1,19 @@
+import "../styles.css";
+
+const Board = () => {
+  return (
+    <div className="board">
+      <div className="top-left-square"></div>
+      <div className="top-mid-square"></div>
+      <div className="top-right-square"></div>
+      <div className="mid-left-square"></div>
+      <div className="mid-mid-square"></div>
+      <div className="mid-right-square"></div>
+      <div className="bottom-left-square"></div>
+      <div className="bottom-mid-square"></div>
+      <div className="bottom-right-square"></div>
+    </div>
+  );
+};
+
+export default Board;
