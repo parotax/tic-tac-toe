35e94b93d2b54bb2e59a49a5fcbd5d1ad8294a6b
Luukas Lehtinen <lukituubi@gmail.com>
2022-11-29 09:32:16 +0200

    Cleanup

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,30 +2,43 @@ import { useState } from "react";
 import "../styles.css";
 
 const Game = () => {
-  const [turn, setTurn] = useState("None");
+  const [turn, setTurn] = useState("Player");
   const board = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0],
   ];
 
-  const handleClick = (tile: number) => {
+  const playerTurn = async (tile: number) => {
     if (turn !== "Player") return;
 
     const x = Math.floor(tile / 3);
     const y = tile % 3;
     console.log(x, y);
-
     if (board[x][y] === 0) {
       board[x][y] = 1;
     } else {
       alert("That tile is already taken!");
       return;
     }
+    setTurn("Computer");
+  };
+
+  const computerTurn = () => {
+    if (board[2][2] === 0) {
+      board[2][2] = 2;
+    }
+    setTurn("Player");
+  };
+
+  const handleClick = async (tile: number) => {
+    playerTurn(tile);
+    computerTurn();
   };
 
   return (
     <>
+      <h2>{turn === "Player" ? "Player's turn" : "Computer's turn"}</h2>
       <button onClick={() => setTurn("Player")}>start</button>
       <button onClick={() => console.log(board)}>board</button>
       <div className="board">
