0c4c3f3b9578b68f41323d76396b57ed81f62b21
Luukas Lehtinen <lukituubi@gmail.com>
2022-12-01 13:11:19 +0200

    fix checkwin

--- src/components/CheckWin.tsx
+++ src/components/CheckWin.tsx
@@ -3,6 +3,7 @@ interface Props {
 }
 
 const checkWin = (props: Props) => {
+  let status = [];
   for (let i = 0; i < 3; i++) {
     let x = 0;
     let y = 0;
@@ -12,10 +13,10 @@ const checkWin = (props: Props) => {
       if (props.board[j][i] === 1) y++;
       else if (props.board[j][i] === 2) y--;
     }
-    if (x === 3) return "Player";
-    if (x === -3) return "Computer";
-    if (y === 3) return "Player";
-    if (y === -3) return "Computer";
+    if (x === 3) status.push("Player");
+    if (y === 3) status.push("Player");
+    if (x === -3) status.push("Computer");
+    if (y === -3) status.push("Computer");
   }
   let diagonal1 = 0;
   let diagonal2 = 0;
@@ -25,10 +26,22 @@ const checkWin = (props: Props) => {
     if (props.board[2 - i][i] === 1) diagonal2++;
     else if (props.board[2 - i][i] === 2) diagonal2--;
   }
-  if (diagonal1 === 3) return "Player";
-  if (diagonal1 === -3) return "Computer";
-  if (diagonal2 === 3) return "Player";
-  if (diagonal2 === -3) return "Computer";
+  if (diagonal1 === 3) status.push("Player");
+  if (diagonal2 === 3) status.push("Player");
+  if (diagonal1 === -3) status.push("Computer");
+  if (diagonal2 === -3) status.push("Computer");
+
+  let tie = true;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (props.board[i][j] === 0) tie = false;
+    }
+  }
+  if (tie) status.push("Tie");
+
+  if (status.includes("Player")) return "Player";
+  if (status.includes("Tie")) return "Tie";
+  if (status.includes("Computer")) return "Computer";
 
   return false;
 };
