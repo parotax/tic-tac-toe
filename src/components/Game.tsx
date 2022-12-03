00b74541f20770729200e6f03c070f38d42376cf
Luukas Lehtinen <lukituubi@gmail.com>
2022-12-03 13:26:46 +0200

    cleaning

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -16,62 +16,55 @@ const Game = () => {
     [0, 0, 0],
   ]);
 
-  const playerTurn = (tile: number) => {
+  const checkWin = () => {
+    let winner = CheckWin({ board });
+    forceUpdate();
+    if (winner === false) return false;
+    alert(winner);
+    return true;
+  };
+
+  const handleTurn = (tile: number) => {
     const x = Math.floor(tile / 3);
     const y = tile % 3;
     if (board[x][y] === 0) {
       const newBoard = board;
       newBoard[x][y] = 1;
       setBoard(newBoard);
-      const won = checkWin();
-      console.log(won)
-      if (!won) return true;
+      if (!checkWin()) return;
+      setBoard(ComputerLogic({ board }));
+      checkWin();
     } else alert("That tile is already taken!");
-    return false;
-  };
-
-  const checkWin = () => {
-    let winner = CheckWin({ board });
-    forceUpdate();
-    if (winner === false) return false;
-    alert(winner);
-    return true
-  };
-
-  const handleClick = (tile: number) => {
-    if (!playerTurn(tile)) return;
-    setBoard(ComputerLogic({ board }));
-    checkWin();
   };
 
   return (
     <>
       <div className="board">
-        <div className="square" onClick={() => handleClick(0)}>
+        <div className="square" onClick={() => handleTurn(0)}>
           <h1>{board[0][0] === 0 ? "" : board[0][0] === 1 ? "X" : "Y"}</h1>
         </div>
-        <div className="square" onClick={() => handleClick(1)}>
+        <div className="square" onClick={() => handleTurn(1)}>
           <h1>{board[0][1] === 0 ? "" : board[0][1] === 1 ? "X" : "Y"}</h1>
         </div>
-        <div className="square" onClick={() => handleClick(2)}>
+        <div className="square" onClick={() => handleTurn(2)}>
           <h1>{board[0][2] === 0 ? "" : board[0][2] === 1 ? "X" : "Y"}</h1>
         </div>
-        <div className="square" onClick={() => handleClick(3)}>
+        <div className="square" onClick={() => handleTurn(3)}>
           <h1>{board[1][0] === 0 ? "" : board[1][0] === 1 ? "X" : "Y"}</h1>
         </div>
-        <div className="square" onClick={() => handleClick(4)}>
+        <div className="square" onClick={() => handleTurn(4)}>
           <h1>{board[1][1] === 0 ? "" : board[1][1] === 1 ? "X" : "Y"}</h1>
         </div>
-        <div className="square" onClick={() => handleClick(5)}>
+        <div className="square" onClick={() => handleTurn(5)}>
           <h1>{board[1][2] === 0 ? "" : board[1][2] === 1 ? "X" : "Y"}</h1>
         </div>
-        <div className="square" onClick={() => handleClick(6)}>
+        <div className="square" onClick={() => handleTurn(6)}>
           <h1>{board[2][0] === 0 ? "" : board[2][0] === 1 ? "X" : "Y"}</h1>
         </div>
-        <div className="square" onClick={() => handleClick(7)}>
+        <div className="square" onClick={() => handleTurn(7)}>
           <h1>{board[2][1] === 0 ? "" : board[2][1] === 1 ? "X" : "Y"}</h1>
         </div>
-        <div className="square" onClick={() => handleClick(8)}>
+        <div className="square" onClick={() => handleTurn(8)}>
           <h1>{board[2][2] === 0 ? "" : board[2][2] === 1 ? "X" : "Y"}</h1>
         </div>
       </div>
